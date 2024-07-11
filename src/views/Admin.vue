d1a84f90a0c0c146a6fc363f9679ceedecc53943
Jaci Reichenberger <jacilr00@gmail.com>
2024-07-11 00:04:08 -0500

    Got everything showing, allows admin to delete users, allows admin to change user roles -jr

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,16 +2,15 @@
 import { onMounted } from "vue";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import CreateStoryServices from "../services/CreateStoryServices";
+import UserServices from "../services/UserServices";
 import StoryExport from "../reports/StoryExport";
 
 const router = useRouter();
 const account = ref(null);
-const content = ref(null);
-const title = ref(null);
-const storyId = ref(null);
+const userId = ref();
 const isDeleted = ref(false);
 const isUserUpdate = ref(false);
+const currentRole = ref(null);
 const userRole = ref(null);
 const snackbar = ref({
   value: false,
@@ -19,6 +18,12 @@ const snackbar = ref({
   text: "",
 });
 
+const userInfo = ref();
+const roles = ref({
+  1: "Administrator",
+  2: "Career Services",
+  3: "Student"
+});
 
 onMounted(async () => {
   account.value = JSON.parse(localStorage.getItem("account"));
@@ -26,79 +31,85 @@ onMounted(async () => {
 });
 
 async function getUsers() {
-  // await CreateStoryServices.getOneStory(storyId.value)
-  //   .then((response) => {
-  //     title.value = response.data.title;
-  //     content.value = response.data.story;
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     snackbar.value.value = true;
-  //     snackbar.value.color = "error";
-  //     snackbar.value.text = error.response.data.message;
-  //   });
+  await UserServices.getUsers()
+    .then((response) => {
+      userInfo.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
 }
 
 //Navigate to Different Pages
-function navigateToView() {
+function navigateToView(itemId) {
+  window.localStorage.setItem("userId", JSON.stringify(itemId));
   router.push({ name: "studentresumeslist" });
 }
 
 //Delete Story
 async function deleteUser() {
-  // await CreateStoryServices.deleteUser(storyId.value, account.value.id)
-  //   .then(() => {
-  //     getTitles();
-  //     localStorage.removeItem("storyId");
-  //     isDeleted.value = false;
-  //     snackbar.value.value = true;
-  //     snackbar.value.color = "green";
-  //     snackbar.value.text = "Story Deleted!";
-  //     router.push({ name: "library" });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     snackbar.value.value = true;
-  //     snackbar.value.color = "error";
-  //     snackbar.value.text = error.response.data.message;
-  //   });
+  await UserServices.deleteAccount(userId.value, account.value.id)
+  .then(() => {
+      getUsers();
+      localStorage.removeItem("userId");
+      isDeleted.value = false;
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "User Deleted!";
+      router.push({ name: "admin" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
   isDeleted.value = false;
 }
-//Delete Story
+//Change User Role
 async function changeUserRole() {
-  // await CreateStoryServices.deleteStory(storyId.value, account.value.id)
-  //   .then(() => {
-  //     getTitles();
-  //     localStorage.removeItem("storyId");
-  //     isDeleted.value = false;
-  //     snackbar.value.value = true;
-  //     snackbar.value.color = "green";
-  //     snackbar.value.text = "Story Deleted!";
-  //     router.push({ name: "library" });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     snackbar.value.value = true;
-  //     snackbar.value.color = "error";
-  //     snackbar.value.text = error.response.data.message;
-  //   });
+  await UserServices.updateAccountRole(account.value.id, userId.value, parseInt(userRole.value))
+    .then(() => {
+      getUsers();
+      localStorage.removeItem("userId");
+      isUserUpdate.value = false;
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "User Role Updated!";
+      router.push({ name: "admin" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
   isUserUpdate.value = false;
 }
 
 function openDelete(itemId) {
-  // window.localStorage.setItem("storyId", JSON.stringify(itemId));
-  // storyId.value = JSON.parse(localStorage.getItem("storyId"));
+  window.localStorage.setItem("userId", JSON.stringify(itemId));
+  userId.value = JSON.parse(localStorage.getItem("userId"));
   isDeleted.value = true;
 }
 
-function openUserUpdate() {
+function openUserUpdate(itemId, roleId) {
+  window.localStorage.setItem("userId", JSON.stringify(itemId));
+  userId.value = JSON.parse(localStorage.getItem("userId"));
+  currentRole.value = String(roleId);
+  userRole.value = String(roleId);
   isUserUpdate.value = true;
 }
 
 function closeUserUpdate() {
+  localStorage.removeItem("userId");
   isUserUpdate.value = false;
 }
 function closeDelete() {
+  localStorage.removeItem("userId");
   isDeleted.value = false;
 }
 
@@ -131,37 +142,17 @@ function closeSnackBar() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="text-left">First</td>
-            <td class="text-left">Last</td>
-            <td class="text-left">email</td>
-            <td class="text-left">Student</td>
-            <td class="text-right">
-              <v-btn rounded variant="text" @click="openUserUpdate()"> Change Role </v-btn>
-              <v-btn rounded variant="text" @click="navigateToView()"> View Resumes </v-btn>
-              <v-btn rounded variant="text" @click="openDelete()"> Delete </v-btn>
-            </td>
-          </tr>
-          <tr>
-            <td class="text-left">First</td>
-            <td class="text-left">Last</td>
-            <td class="text-left">email</td>
-            <td class="text-left">Career Service</td>
-            <td class="text-right">
-              <v-btn rounded variant="text" @click="openUserUpdate()"> Change Role </v-btn>
-              <v-btn rounded variant="text" @click="navigateToView()"> View Resumes </v-btn>
-              <v-btn rounded variant="text" @click="openDelete()"> Delete </v-btn>
-            </td>
-          </tr>
-
-          <!-- <tr v-for="item in titles" :key="item.id">
-            <td class="text-left">{{ item.title }}</td>
-            <td class="text-right">
-              <v-btn rounded variant="text" @click="navigateToView(item.id)"> View </v-btn>
-              <v-btn rounded variant="text" @click="openUserUpdate(item.id)"> Export </v-btn>
+          <tr v-for="item in userInfo" :key="item.id">
+            <td class="text-left" v-if="item.id !== account.id && item.id !== 1">{{ item.firstName }}</td>
+            <td class="text-left" v-if="item.id !== account.id && item.id !== 1">{{ item.lastName }}</td>
+            <td class="text-left" v-if="item.id !== account.id && item.id !== 1">{{ item.email }}</td>
+            <td class="text-left" v-if="item.id !== account.id && item.id !== 1">{{ roles[item.roleId] }}</td>
+            <td class="text-right" v-if="item.id !== account.id && item.id !== 1">
+              <v-btn rounded variant="text" @click="openUserUpdate(item.id, item.roleId)"> Change Role </v-btn>
+              <v-btn rounded variant="text" @click="navigateToView(item.id)"> View Resumes</v-btn>
               <v-btn rounded variant="text" @click="openDelete(item.id)"> Delete </v-btn>
             </td>
-          </tr> -->
+          </tr>
         </tbody>
       </v-table>
 
@@ -170,13 +161,13 @@ function closeSnackBar() {
           <v-card-title class="text-center headline mb-2">Change User Role?</v-card-title>
           <div class="d-flex align-center flex-column pa-6">
           <v-btn-toggle v-model="userRole" variant="outlined" divided>
-            <v-btn>Student</v-btn>
-            <v-btn>Career Services</v-btn>
-            <v-btn>Admin</v-btn>
+            <v-btn value="3">Student</v-btn>
+            <v-btn value="2">Career Services</v-btn>
+            <v-btn value="1">Admin</v-btn>
           </v-btn-toggle>
         </div>
           <v-card-actions>
-            <v-btn variant="flat" color="primary" @click="changeUserRole()">Confirm</v-btn>
+            <v-btn variant="flat" color="primary" :disabled="userRole === currentRole" @click="changeUserRole()">Confirm</v-btn>
             <v-spacer></v-spacer>
             <v-btn variant="flat" color="secondary" @click="closeUserUpdate()">Close</v-btn>
           </v-card-actions>

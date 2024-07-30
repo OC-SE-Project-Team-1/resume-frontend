1b6778b63f44890767dedc507d527f04da274700
Jaci Reichenberger <jacilr00@gmail.com>
2024-07-30 13:00:39 -0500

    minor visual bug fi -jr

--- src/views/CreateResumeInputInfo.vue
+++ src/views/CreateResumeInputInfo.vue
@@ -32,7 +32,6 @@ const tabs = ref();
 const tab = ref("1");
 const resumeTemplate = ref();
 const dialog = ref(false);
-const checkbox1 = ref(false);
 
 const personalInfo = ref();
 const firstName = ref();
@@ -2181,10 +2180,6 @@ export default {
                     <v-spacer></v-spacer>
                 </div>
                 <div align="center">
-                            <v-checkbox
-                                v-model="checkbox1"
-                                :label="'Allow Feedback on this resume'"
-                            ></v-checkbox>
                     <v-btn :disabled="!isGenerated" @click="addResume()">Generate Resume</v-btn>
                 </div>
                 <div align="center">
